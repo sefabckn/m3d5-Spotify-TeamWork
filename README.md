4faae15d0a02285b1b6798a1d18e8a8854438dad
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-03 09:37:32 +0100

    previous project imported

---
+++ README.md
@@ -0,0 +1,3 @@
+# FE-SEP21-BuildWeek1-Spotify
+ We worked as a 3 members team. We used Bootstrap, html and css to create Spotify Website.
+ Javascript and DOM functions used here for functionality; for example, to play the music. 
---
+++ app.js
@@ -0,0 +1,39 @@
+album = []
+
+const albumList = () => {
+        fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem", {
+        "method": "GET",
+    })
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        const obj = {title:"eminem", album: data.data}
+        album.push(obj)
+        console.log("album is here", album)
+        album.forEach((albumResult) => {
+            const data = albumResult.album;
+            data.forEach((result) =>{
+                const title = result.title_short;
+                const cover = result.album.cover_medium;
+                const id = result.album.id;
+                let row = document.querySelector('#recently-played')
+                let col = document.createElement('div')
+                col.className = 'card col-sm-6 col-md-2 px-2 py-2'
+                col.innerHTML +=
+                ` <img class="card__image" src="${cover}" />
+                <div class="card__body">
+                <div class="card__meta">
+                    <p><strong>${title}</strong><span><br>${id}</span></p>
+                </div>
+                </div>`
+                row.appendChild(col)
+            })
+        })
+    })
+}
+
+window.onload = () => {
+    document.querySelector("#album-loader").addEventListener("click", albumList)
+    
+
+}
